948deaa625a9cde7930310718c82e2795e8bc67c
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-24 16:57:42 -0600

    average assignment completion time of the currently enrolled students

--- queries/average_completion_time_of_current_cohort.sql
+++ queries/average_completion_time_of_current_cohort.sql
@@ -1,4 +1,4 @@
-SELECT students.name AS name, avg(assignment_submissions.duration) AS average_assignment_duration
+SELECT students.name AS student, avg(assignment_submissions.duration) AS average_assignment_duration
   FROM students
   LEFT OUTER JOIN assignment_submissions on students.id = student_id
   WHERE students.end_date IS NULL
