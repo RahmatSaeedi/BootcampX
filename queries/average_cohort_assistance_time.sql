7c9f7f251f6e8df437d33ff2e4ebfc1b83afbedf
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-25 15:41:52 -0600

    average duration of assistance requests for each cohort

--- queries/average_cohort_assistance_time.sql
+++ queries/average_cohort_assistance_time.sql
@@ -3,4 +3,4 @@ FROM assistance_requests
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 GROUP BY cohorts.name
-ORDER BY average_assistance_request_duration
+ORDER BY average_assistance_request_duration;
